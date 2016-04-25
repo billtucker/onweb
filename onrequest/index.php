f1fe013ca19b9f6a9dd088f6e7fd26303b7ce531
bill.tucker.la@gmail.com <bill.tucker.la@gmail.com>
2016-04-25 09:42:22 -0700

    Removed validation method from onrequest/index.php since the user will be validated during presentation of the project list.

--- onrequest/index.php
+++ onrequest/index.php
@@ -6,27 +6,13 @@
  * Time: 9:47 AM
  *
  * Note: This page was included as a just in case if some user attempts to reach http:<site_DNS>/onweb/onrequest/
- * redirect the user to the project list page. This actual routes users attempting to use manual URL entry
+ * redirect the user to the project list page. This actual routes users attempting to use manual URL entry. The last
+ * iteration of the 04/22/2016 page had the validation method. That method was removed since the user will be validated
+ * on the project list.
  */
 
 
 include_once("request-config.php");
-
-//validate user prior to access on this page
-include_once("$validation" ."user_validation.php");
-include_once($utilities ."utility.php");
-include_once($errors .'errorProcessing.php');
-
-$pageUrl = urlencode($port ."$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-
-$validateUser = true;
-
-//even though this is a redirect page lets validate them to force a user to login if required
-if($validateUser) {
-    $log->debug("Now validate if Plugin: (" .$onRequestPlugin .") is installed");
-    //validate user then check if plugin license if active to access on this page
-    validateUser($site_prefix, $pageUrl, $siteSection, $onRequestModify, $onRequestPlugin);
-}
 
 //After the user is Validated redirect the user to the Request Project list page or login page
 header('Location: ' . $request_site_prefix . 'projectlist.php');
