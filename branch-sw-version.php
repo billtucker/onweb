6aad93c3b84e14e123918c25adbd03bca1964b0c
bill.tucker.la@gmail.com <bill.tucker.la@gmail.com>
2016-08-05 08:27:39 -0700

    Not sure why I am pushing or pulling this file since its part of gitignore

---
+++ branch-sw-version.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Bill
+ * Date: 8/5/2016
+ * Time: 8:20 AM
+ * This file will be added to the gitignore as this file will reside in branch only but wil populate application
+ * software version of onweb-config.php file
+ */
+
+$branchSoftwareVersion = "onweb-development-Beta-08052016-0000";
