9df6a3d4df0679396e8334b31498673d4a3e9a54
Bill Tucker <bill.tucker.la@gmail.com>
2017-05-09 12:25:05 -0700

    Refactored buildApplicationType function to remove out of place div and added div to approperate place. Cleaned up PHP comments to function in the spotviewer_utility file. Added logging to spotview_controller file.

--- onspot/processing/spotviewer_utility.php
+++ onspot/processing/spotviewer_utility.php
@@ -15,10 +15,12 @@ include_once dirname(__DIR__) .DIRECTORY_SEPARATOR ."onspot-config.php";
 /**
  * The spotviewer will load the appropriate type to support display of the object loaded in the FileMaker Container
  * on the [WEB] cwp_spotviewer_browse (Rough Cut) layout. The processDisplayType method will call out the required
- * method to echo supporting type.
- * @param $url the url to object loaded in FileMaker Container
- * @param $type the type audio, video, image, or application
- * @param $sourceType if required audio/mp3 or video/mp4
+ * @param $url - String URL
+ * @param $type - String sttribute Application, Image, Audio, Video
+ * @param $sourceType - Source Type setting for object display
+ * @param $downloadLink - URL to FileMaker Location
+ * @param $fullVideoLink - URL to object
+ * @param $linkFilename - Actual file name of store object from FileMaker calculation field
  */
 function processDisplayType($url, $type, $sourceType, $downloadLink, $fullVideoLink, $linkFilename){
     global $log;
@@ -129,19 +131,22 @@ function buildApplicationType($url, $linkFilename){
     echo "<div class='row'><!-- Start of special row to apply audio or image link display formating -->" .PHP_EOL;
     echo "<div class=\"col-md-3 col-xs-12\"></div>" .PHP_EOL;
     echo "<div class='col-md-6 col-xs-12 video-audio-border text-center'>" .PHP_EOL;
-    $log->debug("Application - Test if linkFilename is set: " .$linkFilename);
+    $log->debug("Test For linkFilename: " .$linkFilename);
     if(empty($linkFilename)){
         echo "<a class='ignore_button text-center' href='$url' target='_blank' style='margin-top: 10px; margin-bottom: 10px;'>Document From Rough Cut</a>" .PHP_EOL;
     }else{
         echo "<a class='ignore_button text-center' href='$url' target='_blank' style='margin-top: 10px; margin-bottom: 10px;'>$linkFilename</a>" .PHP_EOL;
     }
-
+    echo "</div><!-- End of video, audio, document MD-6 column DIV -->" .PHP_EOL;
     echo "<div class=\"col-md-3 col-xs-12\"></div><!-- Last of empty columns foir audio display -->" .PHP_EOL;
-    echo "</div><!-- add this div to fix audio display issues. -->";
     echo "</div><!-- End of second row definition  -->" .PHP_EOL;
 }
 
-
+/**
+ * A function to display download link to users who access to access the object outside the application
+ * @param $downloadLink - boolean true yes enable
+ * @param $fullVideoLink - string link to object directly
+ */
 function downloadLinkStatus($downloadLink, $fullVideoLink){
     if ($downloadLink) {
         echo "<div style='font-size:10px;'>" .PHP_EOL;
@@ -152,6 +157,9 @@ function downloadLinkStatus($downloadLink, $fullVideoLink){
     echo "</div><!-- downloadlinkstatus method == end of the video link <a> div to set font size -->" .PHP_EOL;
 }
 
+/**
+ * function to generate enpty columns to take up space for centering of object display to user
+ */
 function writeAudioLinkDivSpacer(){
     echo "<div class='row'>" .PHP_EOL;
     echo "<div class='col-md-4 col-xs-12 divider-10'>&nbsp;</div>" .PHP_EOL;
